b6378d0df9e7417984fbae8a4dad53d7f722f9e0
Simon A. F. Lund <os@safl.dk>
2025-04-29 10:28:49 +0200

    refactor: re-do 'setup_files' to explicitly targeting on-disk format
    The setup_files script should now "provoke" the on-disk format for:
    
    * directory FMT_LOCAL, FMT_EXTENTS, FMT_BTREE
    
      - There are additonal cases of FMT_EXTENTS this currently only covers
        one, e.g. how many blocks it spans
      - There are additional cases of FMT_BTREE, e.g. depth of the tree etc.
    
    * file FMT_LOCAL and FMT_EXTENTS
    
      - empty file and very small files (<100 bytes), there are a bunch
        of additonal cases here, such as spanning physical blocks, logical
        blocks etc.
    
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- cijoe/scripts/setup_files.py
+++ cijoe/scripts/setup_files.py
@@ -1,8 +1,10 @@
 """
-Populate a mountpoint with files and dictories of different lengths
-===================================================================
+Populate a mountpoint with files and dictories allocated with different on-disk-format
+======================================================================================
 
-Format the given block device with XFS
+The intent for this script is to create a collection of files and folders which will
+trigger XFS to utilize the different formats: FMT_LOCAL, FMT_EXTENTS, and FMT_BTREE for
+the storage of directory entries and file-content.
 """
 
 import logging as log
@@ -11,58 +13,168 @@ from pathlib import Path
 from cijoe.core.command import Cijoe
 
 
-def populate(args: Namespace, cijoe: Cijoe):
+def create_directory_with_urandom_content(
+    args: Namespace, cijoe: Cijoe, dir: Path, count: int, size: str
+) -> int:
+    """
+    In 'dir' create 'count' files with 'size' amount of content from '/dev/urandom'
+    """
 
-    directories = [
-        ("fld_zero", "1", 3),
-        ("fld_zero", "512", 2),
-        ("fld_zero", "10M", 1),
-        ("fld_zero", "100K", 3),
+    for cmd in [
+        f"mkdir -p {dir}",
+        (
+            f"for ((i=0; i<={count}; i++)); do "
+            f"dd if=/dev/urandom of={dir}/file-rand-{size}-$i-{count}.bin "
+            f"bs={size} count=1"
+            "; done"
+        ),
+    ]:
+        err, state = cijoe.run(cmd)
+        if err:
+            return err
 
-        ("fld_one", "10M", 1),
-        ("fld_one", "100K", 3),
+    return 0
 
-        ("fld_two", "154K", 10),
-        ("fld_two", "500K", 10),
 
-        ("many", "41K", 1200),
-        ("many", "154K", 1200),
-        ("many", "500K", 1200),
-    ]
+def provoke_odf_dir_fmt_local(args: Namespace, cijoe: Cijoe) -> int:
+    """
+    This creates a directory with a few small files in it, which should be able to store
+    the entire contents in locally / inside the inode, that is, use FMT_LOCAL.
 
-    for name, size, count in directories:
-        dir = args.mountpoint / f"{name}"
+    TODO:
 
-        cijoe.run(f"mkdir -p {dir}")
+    * trigger shortform + longform
+    """
 
-        for cur in range(count):
-            filepath = dir / f"file-{name}-{size}-{cur}-{count}.bin"
-            err, state = cijoe.run(
-                f"dd if=/dev/urandom of={filepath} bs={size} count=1"
-            )
+    return create_directory_with_urandom_content(
+        args, cijoe, args.mountpoint / "should-be-dir-fmt_local", 10, "4K"
+    )
+
+
+def provoke_odf_dir_fmt_extents(args: Namespace, cijoe: Cijoe) -> int:
+    """
+    Create a directory with more files than what can fit in FMT_LOCAL, triggering the
+    use of FMT_EXTENTS. This should still not provoke the use of FMT_BTREE, since that
+    would require more fragmentation.
+    """
+
+    return create_directory_with_urandom_content(
+        args, cijoe, args.mountpoint / "should-be-dir-fmt_extents", 100, "4K"
+    )
+
+
+def provoke_odf_dir_fmt_btree(args: Namespace, cijoe: Cijoe) -> int:
+    """
+    Create a directory containing many files requiring the use of FMT_BTREE
+    """
+
+    return create_directory_with_urandom_content(
+        args, cijoe, args.mountpoint / "should-be-dir-fmt_btree", 4000, "4K"
+    )
+
+
+def provoke_odf_file_fmt_local(args: Namespace, cijoe: Cijoe) -> int:
+    """Create empty files, these should be represented as FMT_LOCAL?"""
+
+    prefix = args.mountpoint / "should-be-file-fmt_local"
+
+    total = 10
+    for cur in range(1, total + 1):
+        filepath = prefix / f"empty-{cur}-{total}.file"
+        for cmd in [f"mkdir -p {prefix}", f"touch {filepath}"]:
+            err, _ = cijoe.run(cmd)
             if err:
                 return err
 
     return 0
 
 
-def add_args(parser: ArgumentParser):
-    """Optional function for defining command-line arguments for this script"""
+def provoke_odf_file_fmt_extents(args: Namespace, cijoe: Cijoe) -> int:
+    """Create small files represented as FMT_EXTENTS"""
 
-    parser.add_argument(
-        "--dev-path",
-        type=Path,
-        help="Path to the block device",
-    )
-    parser.add_argument(
-        "--mountpoint",
-        type=Path,
-        help="Path to mountpoint",
-    )
+    path = args.mountpoint / "should-be-file-fmt_extents"
+    for size in [1, 4, 10]:
+        if err := create_directory_with_urandom_content(
+            args, cijoe, path, 1, f"{size}"
+        ):
+            return err
+
+    err, _ = cijoe.run(f"touch {path / 'empty.file'}")
+
+    return err
 
 
-def main(args: Namespace, cijoe: Cijoe):
-    """Entry-point of the cijoe-script"""
+def provoke_odf_file_fmt_btree(args: Namespace, cijoe: Cijoe) -> int:
+    """
+    Creates a file to trigger B+tree on-disk allocation for file content storage.
+
+    This is typically needed due to fragmentation and can be provoked by introducing
+    "holes" during file allocation. Specifically, here 600 calls to `fallocate` allocate
+    4 KB segments at non-contiguous 4 KB offsets. The resulting small file forces B+tree
+    usage, as each gap requires a new extent.
+    """
+
+    prefix = args.mountpoint / "should-be-file-fmt_btree"
+    filepath = prefix / "fragmented"
+
+    commands = [
+        f"mkdir -p {prefix} && truncate -s 0 {filepath}",
+        (
+            f"for ((i=0; i<=600; i++)); do "
+            f"fallocate -o $((i * 8192)) -l 4096 {filepath}"
+            "; done"
+        ),
+    ]
+    for cmd in commands:
+        err, _ = cijoe.run(cmd)
+        if err:
+            return err
+
+    return 0
+
+
+def populate(args, cijoe) -> int:
+    """Explicitly create different files and folders to provoke all ODF cases"""
+
+    if err := provoke_odf_dir_fmt_local(args, cijoe):
+        return err
+
+    if err := provoke_odf_dir_fmt_extents(args, cijoe):
+        return err
+
+    #if err := provoke_odf_dir_fmt_btree(args, cijoe):
+    #    return err
+
+    if err := provoke_odf_file_fmt_local(args, cijoe):
+        return err
+
+    if err := provoke_odf_file_fmt_btree(args, cijoe):
+        return err
+
+    return 0
+
+
+def umount(args: Namespace, cijoe: Cijoe) -> int:
+    """Unmount the file-system at args.mountpoint"""
+
+    err, state = cijoe.run(f"sudo umount {args.mountpoint}")
+    if err:
+        log.error(f"mountpoint({args.mountpoint}); failed un-mounting")
+        return err
+
+    return 0
+
+
+def mount(args: Namespace, cijoe: Cijoe) -> int:
+    """
+    Does the following:
+
+    * Creates directory at 'args.mountpoint' to use as mountpoint; can exist
+    * Mounts 'args.mountpoint' to it
+    * Changes ownership of it any anything inside to $USER
+
+    Returns error in case any of the above fails, or if mountpoint is already mounted.
+    """
 
     err, state = cijoe.run(f"mountpoint {args.mountpoint}")
     if not err:
@@ -84,13 +196,34 @@ def main(args: Namespace, cijoe: Cijoe):
         log.error(f"chown; failed")
         return err
 
-    # Create a bunch of files
+    return 0
+
+
+def add_args(parser: ArgumentParser):
+    """Optional function for defining command-line arguments for this script"""
+
+    parser.add_argument(
+        "--dev-path",
+        type=Path,
+        help="Path to the block device",
+    )
+    parser.add_argument(
+        "--mountpoint",
+        type=Path,
+        help="Path to mountpoint",
+    )
+
+
+def main(args: Namespace, cijoe: Cijoe) -> int:
+    """Entry-point of the cijoe-script"""
+
+    if err := mount(args, cijoe):
+        return err
+
     if err := populate(args, cijoe):
         return err
 
-    err, state = cijoe.run(f"sudo umount {args.mountpoint}")
-    if err:
-        log.error(f"mountpoint({args.mountpoint}); failed un-mounting")
+    if err := umount(args, cijoe):
         return err
 
     return 0
