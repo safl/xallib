1f991ca86c68dc77f690e614059dfc3fc7ba85c7
Simon A. F. Lund <os@safl.dk>
2025-05-15 13:41:01 +0200

    fix(retrieve_dinodes_via_iabt3): add 'seqno' to debug-output
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- src/xal.c
+++ src/xal.c
@@ -1332,16 +1332,17 @@ retrieve_dinodes_via_iabt3(struct xal *xal, struct xal_ag *ag, uint64_t blkno, u
 	iab3.siblings.right = be32toh(iab3.siblings.right);
 	// iab3.blkno = be64toh(iab3.blkno);
 
+	XAL_DEBUG("INFO:    seqno(%" PRIu32 ")", ag->seqno);
 	XAL_DEBUG("INFO:    magic(%.4s, 0x%" PRIx32 ")", iab3.magic.text, iab3.magic.num);
 	XAL_DEBUG("INFO:    level(%" PRIu16 ")", iab3.pos.level);
 	XAL_DEBUG("INFO:  numrecs(%" PRIu16 ")", iab3.pos.numrecs);
-	XAL_DEBUG("INFO:  leftsib(0x%016" PRIx32 " @ %" PRIu64 ")", iab3.siblings.left,
+	XAL_DEBUG("INFO:  leftsib(0x%08" PRIx32 " @ %" PRIu64 ")", iab3.siblings.left,
 		  xal_agbno_absolute_offset(xal, ag->seqno, iab3.siblings.left));
 	
-	XAL_DEBUG("INFO:      bno(0x%016" PRIx64 " @ %" PRIu64 ")", blkno,
+	XAL_DEBUG("INFO:      bno(0x%08" PRIx64 " @ %" PRIu64 ")", blkno,
 		  xal_agbno_absolute_offset(xal, ag->seqno, blkno));
 
-	XAL_DEBUG("INFO: rightsib(0x%016" PRIx32 " @ %" PRIu64 ")", iab3.siblings.right,
+	XAL_DEBUG("INFO: rightsib(0x%08" PRIx32 " @ %" PRIu64 ")", iab3.siblings.right,
 		  xal_agbno_absolute_offset(xal, ag->seqno, iab3.siblings.right));
 
 	if (iab3.pos.level) {
