cb5de295e6e67b453f904685ffa045fbb43ca9ed
Simon A. F. Lund <os@safl.dk>
2025-05-22 22:43:57 +0200

    fix(xal_inode): adjust 'reserved' for alignment
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- include/libxal.h
+++ include/libxal.h
@@ -67,7 +67,7 @@ struct xal_inode {
 	uint8_t ftype;			      ///< File-type (directory, filename, symlink etc.)
 	uint8_t namelen;		      ///< Length of the name; not counting nul-termination
 	char name[XAL_INODE_NAME_MAXLEN + 1]; ///< Name; not including nul-termination
-	uint8_t reserved[30];
+	uint8_t reserved[22];
 	struct xal_inode *parent;
 };
 
