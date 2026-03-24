3f2a693d1536f4338121add6fe77c33c203ed371
Nadja Brix Koch <n.koch@samsung.com>
2026-03-24 15:54:58 +0100

    feat(xal): add xal_from_pools, a shared view of xal pools
    This function creates a minimal xal struct necessary to do lookups after
    the xal_index() has been called. This is helpful when multiple processes
    need to access the same shared memory of the xal pools, but only one has
    and controls the "original" xal struct.
    
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

--- src/xal_pool.c
+++ src/xal_pool.c
@@ -1,10 +1,12 @@
 #define _GNU_SOURCE
 #include <errno.h>
+#include <fcntl.h>
 #include <libxal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <unistd.h>
 #include <xal_pool.h>
 
 int
